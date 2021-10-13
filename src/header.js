6713210d7209368e2c1a14422f42630a5685bcc8
mireu-san <67046863+mireu-san@users.noreply.github.com>
2021-10-13 20:13:10 +0900

    fixed - contact button location issue

--- src/header.js
+++
@@ -1,4 +0,0 @@
-const headerSection = document.querySelector(".header-section")
-const welcome = document.getElementById("welcome")
-const welcomeSub = document.getElementById("welcome-sub")
-
---
+++ src/index.js
@@ -0,0 +1,8 @@
+// https://stackoverflow.com/questions/503093/how-do-i-redirect-to-another-webpage
+
+
+// personal blog
+// window.location.replace("https://silverlibrary.tistory.com/")
+
+// personal kakaotalk redirect link
+// window.location.replace()
