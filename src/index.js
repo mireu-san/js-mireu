25992ba65dfe03a24cd56b424a047ab06432540a
mireu-san <67046863+mireu-san@users.noreply.github.com>
2021-10-14 21:55:20 +0900

    top button will now lead to body__first

--- src/index.js
+++ src/index.js
@@ -1,5 +1,35 @@
 // https://stackoverflow.com/questions/503093/how-do-i-redirect-to-another-webpage
 
+// const topBtnMover = document.querySelector(".intro__button");
+
+// button.addEventListner("click", event => {
+//     button.
+// })
+
+
+// Object.onclick = function(){myScript};
+
+// Object.addEventListener("click", myScript);
+
+// const element = document.querySelector("#post-container");
+
+// element.scrollIntoView({ behavior: 'smooth', block: 'end' });
+
+// let introButton = 'a[href^]"#"]';
+
+// let introButton = document.querySelectorAll(introButton);
+
+// introButton.forEach(link => {
+//     link.onClick = function (e) {
+//         e.preventDefault();
+
+//         let destination = document.querySelector(this.hash);
+
+//         destination.scrollIntoView({
+//             behavior: 'smooth'
+//         });
+//     }
+// })
 
 // personal blog
 // window.location.replace("https://silverlibrary.tistory.com/")
